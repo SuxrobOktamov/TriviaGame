23fda629cc27a54b3c65abc0b1425616d191e83d
Suxrob Oktamov <oktamovsuxrob0@gmail.com>
2023-03-31 17:16:50 +0500

    a new file added

---
+++ script.js
@@ -0,0 +1,24 @@
+
+// console.log(document.domain);
+// console.log(document);
+// console.log(document.URL);
+// console.log(document.title);
+// console.log(document.doctype);
+// console.log(document.head);
+
+//  let java =  document.getElementById("java");
+//  let jav =  document.getElementById("jav") ;
+
+
+//  let button = document.getElementById("btn");
+
+//  button.onclick = function(){
+//     jav.innerText = " I am Suxrob ";
+    
+//  }
+
+// function typp(){
+//     document.getElementById( "java" ).innerHTML  = " Phyton ";
+
+// }
+
