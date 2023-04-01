009f560c1aaf5544f1e657c8170863e2e8d7a1a0
Suxrob Oktamov <oktamovsuxrob0@gmail.com>
2023-04-01 11:12:43 +0500

    the file changed

--- src/Questions.js
+++ src/Questions.js
@@ -8,6 +8,7 @@ function Questions() {
     const [score, setScore] = useState(0);
     const [testOver, setTestOver] = useState(false);
     const [scoreOver, setScoreOver] = useState(false);
+    const [err, setErr] = useState(false);
     const Color = useRef();
     useEffect(()=>{
         setScoreOver(false)
@@ -20,22 +21,12 @@ function Questions() {
     },[])
 
     function handleAnswerClick(answer) {
-
+        setErr(true)
         if(answer) {
             Color.current.className = 'lime';
-            setTimeout(()=>{
-                Color.current.className = '';
-            }, 400)
-        }else {
-            Color.current.className = 'red';
-            setTimeout(()=>{
-                Color.current.className = '';
-            }, 400) 
-        }
-
-        console.log(answer);
-        if (answer) {
             setScore(score + 1);
+        }else {
+            Color.current.className = 'red';    
         }
         setCurrentQuestionIndex(currentQuestionIndex + 1)
         if (currentQuestionIndex === quesArr.length - 1) {
@@ -53,6 +44,11 @@ function Questions() {
             </div>
         )
     }
+    if(err){
+        setTimeout(()=>{
+            Color.current.className = '';
+        }, 400)
+    }
 
     return (
         <div ref={Color}>        
@@ -65,7 +61,7 @@ function Questions() {
                     {quesArr[currentQuestionIndex].incorrect_answers.map(item => <li  key={item}><button onClick={() => handleAnswerClick(false)}>{item}</button> </li>)}
                 </ul>
                 <p className='textScore'>Score {score}</p>
-            </div>: <p className='Loading'> <img  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSbkEUleF3nCVdro2mBkmhWxidHPC_jiW849g&usqp=CAU" alt=""/></p>}
+            </div>: <p className='Loading'> <img  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSbkEUleF3nCVdro2mBkmhWxidHPC_jiW849g&usqp=CAU" alt="img"/></p>}
         </div>
     )
 }
